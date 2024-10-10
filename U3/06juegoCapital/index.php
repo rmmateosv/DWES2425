3d2c0ba967c88e42380ef013998d78577f1be252
Rosa <rmmateosv@gmail.com>
2024-10-10 10:21:29 +0200

    1010

--- U3/06juegoCapital/index.php
+++ U3/06juegoCapital/index.php
@@ -1,19 +1,48 @@
 <?php
+//Añadir un contador de aciertos y de fallos
+//guardando estos datos en la sesión
+//Consideramos que pasar se contabiliza como fallo
+session_start();
 require_once 'Modelo.php';
+
+
+//Si se pulsa en salir se destruye la sesión
+if(isset($_GET['salir']) ){
+    //Elimina sesisón en servidor
+    session_destroy();
+    //Eliminar $_SESSION
+    session_unset();
+}
+//Si no hay noombre no se puede jugar
+if(!isset($_SESSION['nombre'])){
+    header('location:inicio.php');
+}
+
+
 $modelo = new Modelo();
 $preguntasFichero = $modelo->obtenerPreguntas();
 
 if (!isset($_POST['validar']) or isset($_POST['pasar'])) {
     //Si se carga la página o se pulsa en pasar generamos una pregunta aleatoria
-    $num = rand(0, sizeof($preguntasFichero)-1);
+    $num = rand(0, sizeof($preguntasFichero) - 1);
     $pregunta = $preguntasFichero[$num];
+
+    if(isset($_POST['pasar'])){
+        //Incremetamos fallos
+        if(isset($_SESSION['fallos'])){
+            $_SESSION['fallos']++;
+        } 
+        else{
+            $_SESSION['fallos']=1;
+        }
+    }
 } else {
     //Si se ha pulsado en validar hay que mostrar si se ha acertado o no
 
     //Comprobar que se ha rellenado la respuesta
     if (!empty($_POST['respuesta'])) {
         //Variable para recordar el input de la respuesta
-        $respuestaUS=$_POST['respuesta'];
+        $respuestaUS = $_POST['respuesta'];
 
         //Recuperamos la pregunta que se ha contestado a partir del id de la
         //pregunta que está como value en el botón validar
@@ -21,21 +50,35 @@ if (!isset($_POST['validar']) or isset($_POST['pasar'])) {
             if ($p->getId() == $_POST['validar']) {
                 //Creamos dos variables con la pregunta y la respuesta dada por el usuario para
                 //recordar los datos
-                $pregunta=$p;
-               
+                $pregunta = $p;
+
                 //Comprobar si las repuestas son iguales. Comprobamos siempre en minúsculas
                 if (strtolower($p->getCapital()) == strtolower($respuestaUS)) {
-                    $mensaje='<h3 style="color:green;">Correcto</h3>';
-                }
-                else{
-                    $mensaje='<h3 style="color:red;">Incorrecto</h3>';
+                    $mensaje = '<h3 style="color:green;">Correcto</h3>';
+                    //Incremetamos aciertos
+                    if(isset($_SESSION['aciertos'])){
+                        $_SESSION['aciertos']++;
+                    } 
+                    else{
+                        $_SESSION['aciertos']=1;
+                    }
+                    //Recargamos página si acertamos para on pulsar en pasar
+                    //header('location:index.php');
+                } else {
+                    $mensaje = '<h3 style="color:red;">Incorrecto</h3>';
+                    //Incremetamos fallos
+                    if(isset($_SESSION['fallos'])){
+                        $_SESSION['fallos']++;
+                    } 
+                    else{
+                        $_SESSION['fallos']=1;
+                    }
                 }
                 break; //No seguimos comprobando
             }
         }
-    }
-    else{
-        $mensaje='<h3 style="color:red;">Rellena respuesta</h3>';
+    } else {
+        $mensaje = '<h3 style="color:red;">Rellena respuesta</h3>';
     }
 }
 
@@ -51,17 +94,44 @@ if (!isset($_POST['validar']) or isset($_POST['pasar'])) {
 
 <body>
     <h1>JUEGO CAPITAL</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        <div>
-            <h2>¿Cuál es la capital de <?php echo $pregunta->getPais()?>?</h2>
-            <input type="text" id="respuesta" name="respuesta" placeholder="Introduce capital" value="<?php echo (isset($respuestaUS)?$respuestaUS:'')?>"/>
-        </div>
-        <div>
-            <?php echo (isset($mensaje)?$mensaje:'')?>
-        </div>
-        <button type="submit" value="<?php echo $pregunta->getId()?>" name="validar">Validar</button>
-        <button type="submit" name="pasar">Pasar</button>
-    </form>
+    <h2><?php echo $_SESSION['nombre']?></h2>
+    <a href="index.php?salir=1">Salir</a>
+    <table>
+        <tr>
+            <td>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div>
+                        <h2>¿Cuál es la capital de <?php echo $pregunta->getPais() ?>?</h2>
+                        <input type="text" id="respuesta" name="respuesta" placeholder="Introduce capital" value="<?php echo (isset($respuestaUS) ? $respuestaUS : '') ?>" />
+                    </div>
+                    <div>
+                        <?php echo (isset($mensaje) ? $mensaje : '') ?>
+                    </div>
+                    <button type="submit" value="<?php echo $pregunta->getId() ?>" name="validar">Validar</button>
+                    <button type="submit" name="pasar">Pasar</button>
+                </form>
+            </td>
+            <td style="padding-left: 20px;">
+                <?php 
+                    if(isset($_SESSION['aciertos'])){
+                        $aciertos = $_SESSION['aciertos'];
+                    }
+                    else{
+                        $aciertos=0;
+                    }
+                    if(isset($_SESSION['fallos'])){
+                        $fallos = $_SESSION['fallos'];
+                    }
+                    else{
+                        $fallos=0;
+                    }
+                ?>
+                <h2 style="color:green">Aciertos:<?php echo $aciertos?></h2>
+                <h2 style="color:red">Fallos:<?php echo $fallos ?></h2>
+            </td>
+        </tr>
+    </table>
+
 </body>
 
 </html>
