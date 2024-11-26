3c4276c725acccaea559e5e5d7fb9cd0f7bb01ba
rmmateosv <rmmateosvgmail.com>
2024-11-26 11:20:14 +0100

    1121

--- U4/tienda/resources/views/usuarios/login.blade.php
+++ U4/tienda/resources/views/usuarios/login.blade.php
@@ -4,17 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form action="{{route('loguear')}}" method="post">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+    <form action="{{route('loguear')}}" method="post" class="row g-3">
         @csrf
-        Email
-        <input type="email" name="email"/>
-        Contraseña
-        <input type="password" name="ps"/>
-        <button type="submit" name="login">Login</button>
-        <a href="{{route('vistaRegistro')}}">Registrase</a>
+        <div class="row-md-3">
+            <label for="email" class="form-label" >Email</label><br/>
+            <input type="email" name="email" class="form-control"/>
+        </div>
+        <div class="row-md-3">
+            <label for="ps" class="form-label" >Contraseña</label><br/>
+            <input type="password" name="ps" class="form-control"/>
+        </div>
+        <div class="row-md-3">
+            <button type="submit" name="login" class="btn btn-outline-secondary">Login</button>
+            <a href="{{route('vistaRegistro')}}" class="btn btn-outline-secondary">Registrase</a>
+        </div>
     </form>
+    </div>
 </body>
 </html>
--- U4/tienda/resources/views/usuarios/registro.blade.php
+++ U4/tienda/resources/views/usuarios/registro.blade.php
@@ -5,40 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Registro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form action="{{route('registrar')}}" method="post">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+    <form action="{{route('registrar')}}" method="post" class="row g-3">
         @csrf
-        Nombre
-        <input type="text" name="nombre" value="{{old('nombre')}}"/>
+        <div class="row-md-3">
+            <label for="nombre" class="form-label" >Nombre</label>
+        <input type="text" name="nombre" value="{{old('nombre')}}" class="form-control"/>
         @error('nombre')
-            <h3>{{$message}}</h3>
+            <h3 class="text-danger">{{$message}}</h3>
         @enderror
         <br/>
-        Email
-        <input type="email" name="email" value="{{old('email')}}"/>
+        </div>
+        <div class="row-md-3">
+            <label for="email" class="form-label" >Email</label>
+        <input type="email" name="email" value="{{old('email')}}" class="form-control"/>
         @error('email')
             <h3>{{$message}}</h3>
         @enderror
-        <br/>
-        Contraseña
-        <input type="password" name="ps"/>
+        </div>
+        <div class="row-md-3">
+      
+            <label for="ps" class="form-label" >Contraseña</label>
+        <input type="password" name="ps" class="form-control"/>
         @error('ps')
             <h3>{{$message}}</h3>
         @enderror
-        <br/>
-        Confimar Contraseña
-        <input type="password" name="ps2"/>
+        </div>
+        <div class="row-md-3">
+            <label for="ps2" class="form-label" >Confimar Contraseña</label>
+        <input type="password" name="ps2" class="form-control"/>
         @error('ps2')
             <h3>{{$message}}</h3>
         @enderror
-        <br/>
-
-        <button type="submit" name="crearU" value="crearU">Crear</button>
-        <a href="{{route('vistaLogin')}}">Volver</a>
+        </div>
+        <div class="row-md-3">
+        <button type="submit" name="crearU" value="crearU" class="btn btn-outline-secondary">Crear</button>
+        <a href="{{route('vistaLogin')}}" class="btn btn-outline-secondary">Volver</a>
+        </div>
     </form>
     @if (session('mensaje'))
         {{session('mensaje')}}
     @endif
+    </div>
 </body>
 </html>
